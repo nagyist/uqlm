073f1ea10b242f8502e0231cd28b0b4cfd4d724a
Dylan Bouchard <dylan.bouchard@cvshealth.com>
2025-10-21 18:29:24 +0000

    add monte carlo entropy and CoCoA scorers

---
+++ uqlm/white_box/multi_generation.py
@@ -0,0 +1,48 @@
+# Copyright 2025 CVS Health and/or one of its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import numpy as np
+from typing import List, Dict, Any, Optional, Union, Callable
+from uqlm.black_box.cosine import CosineScorer
+
+
+class MultiGenerationScorer(WhiteBoxScorer):
+    def __init__(self):
+        pass
+    
+    def compute_consistency_confidence(self, logprobs_results: List[List[Dict[str, Any]]], responses: List[str], sampled_responses: List[List[str]]) -> List[float]:
+        cosine_scores = CosineScorer().evaluate(responses=responses, sampled_responses=sampled_responses)
+        response_probs = self._compute_single_generation_scores(logprobs_results, self._norm_prob)
+        cocoa_scores = [cs * rp for cs, rp in zip(cosine_scores, response_probs)]
+        return cocoa_scores
+    
+    def compute_monte_carlo_sequence_entropy(
+        self, 
+        logprobs_results: List[List[Dict[str, Any]]], 
+        sampled_logprobs_results: List[List[List[Dict[str, Any]]]], 
+        responses: List[str], 
+        sampled_responses: List[List[str]]
+    ) -> List[float]:
+        num_responses = len(sampled_responses[0]) + 1
+        monte_carlo_negentropy_scores = []
+        for i in range(len(responses)):
+            all_logprobs_response_i = [logprobs_results[i]] + sampled_logprobs_results[i]
+            all_responses_i = [responses[i]] + sampled_responses[i]
+            
+            all_sampled_sequence_probs_response_i = self._compute_single_generation_scores(all_logprobs_response_i, self._norm_prob)
+            monte_carlo_entropy_i = self._entropy_from_probs(probs_list=all_sampled_sequence_probs_response_i, texts=all_responses_i)
+            monte_carlo_negentropy_i = 1 - monte_carlo_entropy_i / num_responses
+            monte_carlo_negentropy_scores.append(monte_carlo_negentropy_i)
+        return monte_carlo_negentropy_scores
