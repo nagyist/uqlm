bb763134aff22d204edd08c9964ee459a8d6e548
2613037 <mohitcek@gmail.com>
2025-10-21 11:02:33 -0400

    update async-await statements

--- uqlm/longform/black_box/claimqa.py
+++ uqlm/longform/black_box/claimqa.py
@@ -1,3 +1,4 @@
+import asyncio
 import contextlib
 import io
 import re
@@ -22,9 +23,9 @@ class ClaimQAScorer:
         response_template: str = "atomic",
         device: Any = None,
         system_prompt: str = "You are a helpful assistant.",
+        sampling_temperature: float = 1.0,
         max_calls_per_min: int = 1000,
         use_n_param: bool = False,
-        num_factoids: int = 7,
         num_questions: int = 2,
         num_claim_qa_responses: int = 2,
     ):
@@ -43,12 +44,12 @@ class ClaimQAScorer:
             The template to use for generating responses. Choose from "atomic" or "factoid".
         system_prompt : str
             The system prompt to use for generating responses. Default is "You are a helpful assistant."
+        sampling_temperature : float, default=1.0
+            The temperature to use for sampling the responses.
         max_calls_per_min : int
             The maximum number of calls per minute to the LLM.
         use_n_param : bool
             Whether to use the n parameter for the LLM.
-        num_factoids : int, default=7
-            The number of factoids to generate for each response.
         num_questions : int, default=2
             The number of questions to generate for each factoid.
         num_claim_qa_responses : int, default=2
@@ -57,11 +58,10 @@ class ClaimQAScorer:
         self.llm = llm
         self.llm_decomposer = llm_decomposer if llm_decomposer is not None else llm
         self.llm_questioner = llm_questioner if llm_questioner is not None else self.llm_decomposer
-        self.bb_object = BlackBoxUQ(llm=llm, scorers=black_box_scorers, device=device, max_calls_per_min=max_calls_per_min)
+        self.bb_object = BlackBoxUQ(llm=llm, scorers=black_box_scorers, device=device, max_calls_per_min=max_calls_per_min, sampling_temperature=sampling_temperature)
         self.system_prompt = system_prompt
         self.max_calls_per_min = max_calls_per_min
         self.use_n_param = use_n_param
-        self.num_factoids = num_factoids
         self.num_questions = num_questions
         self.num_claim_qa_responses = num_claim_qa_responses
         if response_template == "atomic":
@@ -83,8 +83,9 @@ class ClaimQAScorer:
         self.response_scores = {key: [] for key in self.bb_object.scorers}
         self.response_scores, self.factoid_scores = {key: [] for key in self.bb_object.scorers}, {key: [] for key in self.bb_object.scorers}
         self.response_fact_questions, self.response_fact_questions_responses, self.response_fact_questions_sampled_responses = [], [], []
-        for i in range(len(responses)):
-            tmp = await self._compute_factoid_scores(original_prompt=self.prompts[i], original_response=responses[i], factoids=self.factoids[i])
+        tasks = [self._compute_factoid_scores(original_prompt=self.prompts[i], original_response=responses[i], factoids=self.factoids[i]) for i in range(len(responses))]
+        factoid_results = await asyncio.gather(*tasks)
+        for tmp in factoid_results:
             for key in self.response_scores:
                 self.response_scores[key].append(np.mean(tmp["factoid_scores"][key]))
                 self.factoid_scores[key].append(tmp["factoid_scores"][key])
@@ -93,9 +94,6 @@ class ClaimQAScorer:
             self.response_fact_questions_sampled_responses.append(tmp["factoid_questions_sampled_responses"])
             if progress_bar:
                 progress_bar.update(progress_task, advance=1)
-        # print("Factoids: ", self.factoids)
-        # print(" ")
-        # print("Response-level scores: ", self.response_scores)
         return self._construct_result()
 
     async def generate_and_score(self, prompts: List[str], progress_bar: Optional[Progress] = None):
@@ -112,8 +110,6 @@ class ClaimQAScorer:
         self.prompts = prompts
         responses = await self._generate_responses(llm=self.llm, prompts=self.prompts, count=1, progress_bar=progress_bar)
         self.responses = responses["responses"]
-        print("Prompts: ", self.prompts)
-        print("Responses: ", self.responses)
         return await self.score(prompts=self.prompts, responses=self.responses, progress_bar=progress_bar)
 
     async def score(self, prompts: List[str], responses: List[str], progress_bar: Optional[Progress] = None):
@@ -135,27 +131,11 @@ class ClaimQAScorer:
 
         # TODO: Appropriately implement self.num_factoids to generate the number of claims/factoids
         decomposer = ResponseDecomposer(claim_decomposition_llm=self.llm_decomposer, response_template=self.response_template)
-        self.factoids = []
-        for response in responses:
-            self.factoids += await decomposer.decompose_claims(responses=[response], progress_bar=progress_bar)
+        
+        tasks = [decomposer.decompose_claims(responses=[response], progress_bar=progress_bar) for response in responses]
+        tmp = await asyncio.gather(*tasks)
+        self.factoids = [t[0] for t in tmp]
 
-        # if progress_bar:
-        #     progress_task = progress_bar.add_task("  - Computing ClaimQA Score...", total=len(self.factoids))
-        # self.response_scores, self.factoid_scores = {key: [] for key in self.bb_object.scorers}, {key: [] for key in self.bb_object.scorers}
-        # self.response_fact_questions, self.response_fact_questions_responses, self.response_fact_questions_sampled_responses = [], [], []
-        # for i, response in enumerate(responses):
-        #     tmp = await self._compute_factoid_scores(original_prompt=self.prompts[i], original_response=responses[i], factoids=self.factoids[i])
-        #     for key in self.response_scores:
-        #         self.response_scores[key].append(np.mean(tmp["factoid_scores"][key]))
-        #         self.factoid_scores[key].append(tmp["factoid_scores"][key])
-        #     self.response_fact_questions.append(tmp["factoid_questions"])
-        #     self.response_fact_questions_responses.append(tmp["factoid_questions_responses"])
-        #     self.response_fact_questions_sampled_responses.append(tmp["factoid_questions_sampled_responses"])
-        #     if progress_bar:
-        #         progress_bar.update(progress_task, advance=1)
-        # print("Factoids: ", self.factoids)
-        # print(" ")
-        # print("Response-level scores: ", self.response_scores)
         return await self.evaluate(prompts=self.prompts, responses=responses, factoids=self.factoids, progress_bar=progress_bar)
 
     async def _compute_factoid_scores(self, original_prompt: str, original_response: str, factoids: List[List[str]]) -> List[float]:
@@ -180,7 +160,7 @@ class ClaimQAScorer:
         factoid_questions, factoid_questions_responses, factoid_questions_sampled_responses = [], [], []
         # print("Factoids for ith response: ", factoids)
         # print(" ")
-        for factoid_i in factoids[: self.num_factoids]:  # TODO: Appropriately implement self.num_factoids to generate the number of claims/factoids
+        for factoid_i in factoids:
             # Generate questions on each factoid
             create_question_prompt = get_question_template(original_response, factoid_i, self.num_questions)
             # print("Prompt: ", create_question_prompt)
@@ -246,7 +226,12 @@ class ClaimQAScorer:
         response_fact_questions_responses = getattr(self, "response_fact_questions_responses", [])
         response_fact_questions_sampled_responses = getattr(self, "response_fact_questions_sampled_responses", [])
         factoid_scores = getattr(self, "factoid_scores", [])
-        data = {"prompts": prompts, "responses": responses, "response_scores": response_scores, "factoid_scores": factoid_scores}
+        data = {"prompts": prompts, "responses": responses}
+        tmp = {}
+        for key in response_scores:
+            tmp["response_scores_"+key] = response_scores[key]
+            tmp["factoid_scores_"+key] = factoid_scores[key]
+        data.update(tmp)
         metadata = {"factoids": self.factoids, "response_fact_questions": response_fact_questions, "response_fact_questions_responses": response_fact_questions_responses, "response_fact_questions_sampled_responses": response_fact_questions_sampled_responses}
         result = {"data": data, "metadata": metadata}
         return UQResult(result)
