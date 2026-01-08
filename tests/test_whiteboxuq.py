9eadd9f9fdc74c95fe381e72774dedfe04a021ae
Dylan Bouchard <dylan.bouchard@cvshealth.com>
2026-01-08 15:43:51 +0000

    update white box uq test

--- tests/test_whiteboxuq.py
+++ tests/test_whiteboxuq.py
@@ -33,7 +33,7 @@ mock_object = AzureChatOpenAI(deployment_name="YOUR-DEPLOYMENT", temperature=1.0
 
 @pytest.mark.asyncio
 async def test_whiteboxuq_basic(monkeypatch):
-    wbuq = WhiteBoxUQ(llm=mock_object, scorers=["normalized_probability", "min_probability"])
+    wbuq = WhiteBoxUQ(llm=mock_object, scorers=["sequence_probability", "min_probability"])
 
     async def mock_generate_original_responses(*args, **kwargs):
         wbuq.logprobs = MOCKED_LOGPROBS
@@ -45,7 +45,7 @@ async def test_whiteboxuq_basic(monkeypatch):
         results = await wbuq.generate_and_score(prompts=PROMPTS, show_progress_bars=show_progress_bars)
 
         for i in range(len(PROMPTS)):
-            assert results.data["normalized_probability"][i] == pytest.approx(data["normalized_probability"][i])
+            assert results.data["sequence_probability"][i] == pytest.approx(data["normalized_probability"][i])
             assert results.data["min_probability"][i] == pytest.approx(data["min_probability"][i])
 
         assert results.metadata == metadata
